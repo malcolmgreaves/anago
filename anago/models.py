64a8649088865087b23cd148657277ddc4190494
Malcolm Greaves <greaves.malcolm@gmail.com>
2018-02-14 17:10:53 -0800

    attempt to make sure shapes align

--- anago/models.py
+++ anago/models.py
@@ -56,19 +56,47 @@ class SeqLabeling(BaseModel):
                                         weights=[embeddings])(word_ids)
 
         # build character based word embedding
-        char_ids = Input(batch_shape=(None, None, None), dtype='int32')
+        char_ids = Input(batch_shape=(None, None), dtype='int32')
         char_embeddings = Embedding(input_dim=config.char_vocab_size,
                                     output_dim=config.char_embedding_size,
                                     mask_zero=True
                                     )(char_ids)
-        s = K.shape(char_embeddings)
-        char_embeddings = Lambda(lambda x: K.reshape(x, shape=(-1, s[-2], config.char_embedding_size)))(char_embeddings)
+        print("\n\n\n\n")
+        print(char_embeddings._keras_shape)
+        print("\n\n\n\n")
+
+        c_s = K.shape(char_embeddings)
+
+        char_embeddings = Lambda(lambda x: K.reshape(x, shape=(-1, c_s[-2], config.char_embedding_size)))(char_embeddings)
+        print("\n\n\n\n")
+        print(char_embeddings._keras_shape)
+        print("\n\n\n\n")
 
         fwd_state = LSTM(config.num_char_lstm_units, return_state=True)(char_embeddings)[-2]
         bwd_state = LSTM(config.num_char_lstm_units, return_state=True, go_backwards=True)(char_embeddings)[-2]
+
+        import ipdb
+        ipdb.set_trace()
+
         char_embeddings = Concatenate(axis=-1)([fwd_state, bwd_state])
+        print("\n\n\n\n")
+        print(char_embeddings._keras_shape)
+        print("\n\n\n\n")
         # shape = (batch size, max sentence length, char hidden size)
-        char_embeddings = Lambda(lambda x: K.reshape(x, shape=[-1, s[1], 2 * config.num_char_lstm_units]))(char_embeddings)
+
+        import ipdb
+        ipdb.set_trace()
+
+        # char_embeddings = Lambda(lambda x: K.reshape(x, shape=(-1, c_s[1], 2 * config.num_char_lstm_units)))(char_embeddings)
+        char_embeddings = Lambda(lambda x: K.reshape(x, shape=(-1, c_s[-2], 2 * config.num_char_lstm_units)),
+                                 output_shape=(None, 2 * config.num_char_lstm_units))(char_embeddings)
+        print("\n\n\n\n")
+        print(char_embeddings._keras_shape)
+        print("\n\n\n\n")
+
+        _shape = c_s
+        import ipdb
+        ipdb.set_trace()
 
         # combine characters and word
         x = Concatenate(axis=-1)([word_embeddings, char_embeddings])
